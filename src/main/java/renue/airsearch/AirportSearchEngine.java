f441c0704933ec7a25776f17b746ee42b3961a53
vudovenko <vladislav.udovenko0@yandex.ru>
2023-05-08 03:02:10 +0500

    added search for the desired airports

--- src/main/java/renue/airsearch/AirportSearchEngine.java
+++ src/main/java/renue/airsearch/AirportSearchEngine.java
@@ -3,6 +3,7 @@ package renue.airsearch;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class AirportSearchEngine {
 
@@ -15,7 +16,79 @@ public class AirportSearchEngine {
         Scanner scanner = new Scanner(System.in);
         getAirportNamesAndByteNumbers();
         Collections.sort(names);
+    }
+
+//    public static List<String> getDesiredAirports(List<String> names, String subname) {
+//        int startIndex = binarySearchFirstString(names, subname);
+//        int endIndex = binarySearchLastString(names, subname);
+//
+//        if (startIndex != -1 && endIndex != -1) {
+//            for (int i = startIndex; i <= endIndex; i++) {
+//                System.out.println(names.get(i));
+//            }
+//        } else {
+//            System.out.println("Подстрока не найдена");
+//        }
+//    }
+
+    public static void startRequestingAirportNames(Scanner scanner) {
         String airportName = getBeginningAirportName(scanner);
+        while (!airportName.equals("!quit")) {
+            long numberAirports = 0;
+            long startTime = System.nanoTime();
+
+
+            long endTime = System.nanoTime();
+            long milliseconds = TimeUnit.NANOSECONDS.toMillis(endTime - startTime);
+        }
+    }
+
+    // Бинарный поиск самой первой строки, содержащей подстроку
+    private static int binarySearchFirstString(List<String> names, String subname) {
+        int low = 0;
+        int high = names.size() - 1;
+        int result = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            String currentString = names.get(mid);
+
+            if (currentString.toLowerCase().startsWith(subname.toLowerCase())) {
+                // Нашли строку, начинающуюся с подстроки
+                result = mid;
+                high = mid - 1; // Переходим к поиску в левой половине
+            } else if (currentString.toLowerCase().compareTo(subname.toLowerCase()) < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    // Бинарный поиск самой последней строки, содержащей подстроку
+    private static int binarySearchLastString(List<String> names, String subnames) {
+        int low = 0;
+        int high = names.size() - 1;
+        int result = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            String currentString = names.get(mid);
+
+            if (currentString.toLowerCase().startsWith(subnames.toLowerCase())) {
+                // Нашли строку, начинающуюся с подстроки
+                result = mid;
+                low = mid + 1; // Переходим к поиску в правой половине
+            } else if (currentString.toLowerCase().compareTo(subnames.toLowerCase()) < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return result;
     }
 
     public static String getBeginningAirportName(Scanner scanner) {
